af08e5e6940ab4ecb0b6225d543a78859ed6ed7b
Sahil <sahilverma1931@gmail.com>
2025-04-01 08:41:55 +0530

    Add missing code to load env file in main.go

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"log"
 	"os"
-	"url-shortener/routes"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env variables")
+	}
+
 	app := fiber.New()
 	app.Use(logger.New())
 
-	routes.SetupRoutes(app)
+	// routes.SetupRoutes(app)
 
 	PORT := os.Getenv("PORT")
 
